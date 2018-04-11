c513f5994f2225e01cf03e355406691935950949
freedomkhanyile <Freedom.Khanyile1@gmail.com>
2018-04-11 22:32:04 +0200

    Added the Portal aspect of the system using Material Bootstrap design

---
+++ portal/index.php
@@ -0,0 +1,91 @@
+﻿<?php require 'head.php';?>		
+<?php require 'nav.php';?>
+	<div class="header"><h4 class="title"><i class="pe-7s-timer"></i> System Capabilities</h4></div>
+	<div class="content all-icons">
+		  <div class="row">
+				<div class="font-icon-list col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                       <div class="font-icon-detail"><i class="pe-7s-credit"></i>
+                         <input type="text" value="Manage Bookings">
+                       </div>
+                </div>	
+				<div class="font-icon-list col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                       <div class="font-icon-detail"><i class="pe-7s-car"></i>
+                         <input type="text" value="Manage Vehicles">
+                       </div>
+                </div>
+				<div class="font-icon-list col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                       <div class="font-icon-detail"><i class="pe-7s-map-marker"></i>
+                         <input type="text" value="Manage Hire Points">
+                       </div>
+                </div>		 
+		  
+				<div class="font-icon-list col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                       <div class="font-icon-detail"><i class="pe-7s-cash"></i>
+                         <input type="text" value="Manage Pricing and Discounts">
+                       </div>
+                </div>				 
+		  </div>
+		  <div class="row">		   
+              <div class="col-md-12">                         
+                            <div class="header">
+                                <h4 class="title"><i class="pe-7s-map-2"></i> Active Rentals </h4>
+                                <p class="category">Active Rentals in the System</p>
+                            </div>
+                            <div class="content table-responsive table-full-width">
+                                <table class="table table-hover table-striped">
+                                    <thead>
+                                        <tr><th>Customer ID</th>
+                                    	<th>Name</th>
+                                    	<th>Amount</th>
+                                    	<th>Vehicle</th>
+										<th>Hire Date</th>
+										<th>Drop Off Date</th>
+                                    	<th>Hire Point</th>
+										<th>Drop Off Point</th>
+										<td>AudoMeter</td>
+                                    </tr></thead>
+                                    <tbody>
+                                        <tr>
+                                        	<td>1</td>
+                                        	<td>Jack Reacher</td>
+                                        	<td>R3680.00</td>
+                                        	<td>VW Golf 7.5 GTI</td>
+                                        	<td>2018/04/11</td>
+											<td>2018/04/30</td>
+											<td>Randburg South Africa</td>
+											<td>Durban South Africa</td>
+											<td>699 -KM</td>
+                                        </tr>   
+										 <tr>
+                                        	<td>2</td>
+                                        	<td>James Bond</td>
+                                        	<td>R4000.00</td>
+                                        	<td>Mercedes AMG C63</td>
+                                        	<td>2018/04/11</td>
+											<td>2018/04/30</td>
+											<td>Randburg South Africa</td>
+											<td>Durban South Africa</td>
+											<td>0 -KM</td>
+                                        </tr> 
+										 <tr>
+                                        	<td>3</td>
+                                        	<td>Casper Nyovest</td>
+                                        	<td>R50000.00</td>
+                                        	<td>Bugatti Veyron</td>
+                                        	<td>2018/04/11</td>
+											<td>2018/04/30</td>
+											<td>Randburg South Africa</td>
+											<td>Durban South Africa</td>
+											<td>1000 -KM</td>
+                                        </tr> 
+                                        
+                                    </tbody>
+                         </table>
+                   </div>                  
+              </div>
+		  
+		  </div>
+	 </div>
+	 
+<?php require 'footer.php';?>	
+	
--- web/header.php
+++ web/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Wacky Trip a Travel Agency Category Bootstrap responsive Website Template | Home :: w3layouts</title>
+<title>Camellogs</title>
 <!-- meta-tags -->
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
